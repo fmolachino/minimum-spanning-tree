bdcc00770c88ce5ff24ba230494746376324a95a
fmolachino <fmolachino@gmail.com>
2023-05-09 21:11:23 -0300

    cleaned unused method

--- src/main/java/com/example/mst3/View.java
+++ src/main/java/com/example/mst3/View.java
@@ -148,14 +148,4 @@ public class View {
         return provinciaField.getText();
     }
 
-    /*public void alertMessage(String header, String body){
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Error");
-        alert.setHeaderText(header);
-        alert.setContentText(body);
-        alert.showAndWait();
-    }*/
-
-
-
 }
